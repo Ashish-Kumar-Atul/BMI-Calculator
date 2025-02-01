65d5219a207458c481ec477a898da3f15cbe479e
Ashish Kumar Atul <162558245+Ashish-Kumar-Atul@users.noreply.github.com>
2025-02-01 19:23:22 +0530

    Added Content Loader

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const heightInput = document.getElementById('height');
-const weightInput = document.getElementById('weight');
-const submit = document.getElementById('submit');
-const result = document.getElementById('result');
-
-
-submit.addEventListener('click', function () {
-    const height = heightInput.value;
-    const weight = weightInput.value;
-        if (!height || !weight) {
-            alert('Please enter both height and weight!');
-            return;
-        }
-
-        // console.log('Height:', height);
-        // console.log('Weight:', weight);
-        const bmi = (weight / ((height * height) / 10000)).toFixed(2);
-        result.innerHTML = `<span>Your BMI : ${bmi}</span>`;
-});
+document.addEventListener('DOMContentLoaded',() => {
+    const heightInput = document.getElementById('height');
+    const weightInput = document.getElementById('weight');
+    const submit = document.getElementById('submit');
+    const result = document.getElementById('result');
+    
+    
+    submit.addEventListener('click', function () {
+        const height = heightInput.value;
+        const weight = weightInput.value;
+            if (!height || !weight) {
+                alert('Please enter both height and weight!');
+                return;
+            }
+            
+            const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+            result.innerHTML = `<span>Your BMI : ${bmi}</span>`;
+    });
+})
